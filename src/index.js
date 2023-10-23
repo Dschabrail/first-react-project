55252ca7096b56883a9b962100a81a1135ae7845
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2023-10-23 22:14:05 +0200

    added bootstrap

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Navbar from './components/navbar';
+import Product from './components/product';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ReactDom.render(<Product/>, document.getElementById('root'));
